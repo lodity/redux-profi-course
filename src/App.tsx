6540d1fd8324fde2fbe72a3e96b5be6fea6b0568
lodity <lodity9@gmail.com>
2022-09-29 15:56:06 +0300

    redux toolkit: slice, extraReducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { fetchUsers } from './store/reducers/ActionCreators';
 
 function App() {
-  return <div className="App"></div>;
+	const dispatch = useAppDispatch();
+	const { error, isLoading, users } = useAppSelector(
+		(state) => state.userReducer
+	);
+
+	useEffect(() => {
+		dispatch(fetchUsers());
+	}, []);
+
+	return (
+		<div className="App">
+			{isLoading && <h1>Loading...</h1>}
+			{error && <h1>{error}</h1>}
+			{JSON.stringify(users, null, 2)}
+		</div>
+	);
 }
 
 export default App;
