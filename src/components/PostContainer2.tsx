a92861343d18df5264e190306f55df09184063e3
lodity <lodity9@gmail.com>
2022-09-29 21:18:22 +0300

    end of video (mutations)

--- src/components/PostContainer2.tsx
+++ src/components/PostContainer2.tsx
@@ -3,14 +3,18 @@ import { postAPI } from '../services/PostService';
 import PostItem from './PostItem';
 
 const PostContainer2 = () => {
-	const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(10);
+	const {
+		data: posts,
+		error,
+		isLoading,
+	} = postAPI.useFetchAllPostsQuery(100);
 	return (
 		<div>
 			<div className="post__list">
 				{isLoading && <h1>Loading...</h1>}
 				{error && <h1>Error!</h1>}
-				{posts &&
-					posts.map((post) => <PostItem key={post.id} post={post} />)}
+				{/*{posts &&*/}
+				{/*	posts.map((post) => <PostItem key={post.id} post={post} />)}*/}
 			</div>
 		</div>
 	);
--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -4,8 +4,9 @@ import { IPost } from '../models/IPost';
 export const postAPI = createApi({
 	reducerPath: 'postAPI',
 	baseQuery: fetchBaseQuery({
-		baseUrl: 'https://jsonplaceholder.typicode.com',
+		baseUrl: 'http://localhost:5000',
 	}),
+	tagTypes: ['Post'],
 	endpoints: (build) => ({
 		fetchAllPosts: build.query<IPost[], number>({
 			query: (limit: number = 5) => ({
@@ -14,6 +15,34 @@ export const postAPI = createApi({
 					_limit: limit,
 				},
 			}),
+			// Обеспечивание доставки данных
+			providesTags: (result) => ['Post'],
+		}),
+		createPost: build.mutation<IPost, IPost>({
+			query: (post) => ({
+				url: '/posts',
+				method: 'POST',
+				body: post,
+			}),
+			// Указываем, что данные становятся неактуальными
+			invalidatesTags: ['Post'],
+		}),
+		updatePost: build.mutation<IPost, IPost>({
+			query: (post) => ({
+				url: `/posts/${post.id}`,
+				method: 'PUT',
+				body: post,
+			}),
+			// Указываем, что данные становятся неактуальными
+			invalidatesTags: ['Post'],
+		}),
+		deletePost: build.mutation<IPost, IPost>({
+			query: (post) => ({
+				url: `/posts/${post.id}`,
+				method: 'DELETE',
+			}),
+			// Указываем, что данные становятся неактуальными
+			invalidatesTags: ['Post'],
 		}),
 	}),
 });
